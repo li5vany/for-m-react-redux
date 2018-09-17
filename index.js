c28af9366f5c4d6864f17c169732447a8faea432
Lisvany Alonso Martinez <7i5vany@gmail.com>
2018-09-17 17:01:05 -0400

    Exports components

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-import FormReactRedux from './src/FormReactRedux';
-module.exports = FormReactRedux;
+import FormReactRedux, {getValueFromFormReactRedux, getUniqueKey} from './src/FormReactRedux';
+import reducers from './src/reduxcers/index';
+import * as types from './src/types';
+
+module.exports = {FormReactRedux, getValueFromFormReactRedux, getUniqueKey, reducers, types};
